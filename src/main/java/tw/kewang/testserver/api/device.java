b5d3e01135cc5b4384af3742a14742145e150a80
furry <accept441@gmail.com>
2017-10-31 09:05:07 +0800

    first stage: device.java

--- src/main/java/tw/kewang/testserver/api/device.java
+++ src/main/java/tw/kewang/testserver/api/device.java
@@ -1,4 +1,27 @@
 package tw.kewang.testserver.api;
 
+import com.google.gson.Gson;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+@Path("device")
 public class device {
+    private static final Gson GSON = new Gson();
+    @Path("{time}")
+    @POST
+    public Response post(@PathParam("time") String time, String body) {
+        DetectData detectData = new DetectData();
+        detectData = GSON.fromJson(body,detectData.getClass());
+        System.out.println(detectData);
+        return null;
+    }
+
 }
