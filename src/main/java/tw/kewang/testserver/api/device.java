952375d37af5440c2fab3419d747f7729add333a
accept441 <accept441@gmail.com>
2017-10-31 09:51:39 +0800

    service first version

--- src/main/java/tw/kewang/testserver/api/device.java
+++ src/main/java/tw/kewang/testserver/api/device.java
@@ -5,10 +5,7 @@ import com.google.gson.Gson;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
+import tw.kewang.testserver.DAO.Service3DAO;
 
 import java.io.IOException;
 
@@ -18,10 +15,11 @@ public class device {
 
     @Path("{time}")
     @POST
-    public Response post(@PathParam("time") String time, String body) {
+    public Response post(@PathParam("time") String time, String body) throws IOException {
         DetectData detectData = new DetectData();
         detectData = GSON.fromJson(body, detectData.getClass());
-        System.out.println(detectData);
+        Service3DAO service3DAO = new Service3DAO();
+        service3DAO.putData(time, detectData);
         return null;
     }
 
