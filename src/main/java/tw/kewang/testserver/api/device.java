a3a350004bf8653a08dba013edc1255398c23674
jamesbaloney <jamesbaloney@mitake.com.tw>
2017-10-31 09:36:02 +0800

    reset lib

--- src/main/java/tw/kewang/testserver/api/device.java
+++ src/main/java/tw/kewang/testserver/api/device.java
@@ -1,11 +1,5 @@
 package tw.kewang.testserver.api;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class device {
-=======
-=======
->>>>>>> a900341ed2edd8982bd33a2a21999e33034199b5
 import com.google.gson.Gson;
 
 import javax.ws.rs.*;
@@ -21,20 +15,16 @@ import java.io.IOException;
 @Path("device")
 public class device {
     private static final Gson GSON = new Gson();
+
     @Path("{time}")
     @POST
     public Response post(@PathParam("time") String time, String body) {
         DetectData detectData = new DetectData();
-        detectData = GSON.fromJson(body,detectData.getClass());
+        detectData = GSON.fromJson(body, detectData.getClass());
         System.out.println(detectData);
         return null;
     }
 
-<<<<<<< HEAD
->>>>>>> device
-=======
-=======
-public class device {
->>>>>>> fecf8af6c4739d93e87086c1e3d8c7a286030835
->>>>>>> a900341ed2edd8982bd33a2a21999e33034199b5
 }
+
+
