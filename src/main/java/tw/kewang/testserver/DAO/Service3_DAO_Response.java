86eb0a4b2ea3e890b617fef00063f8a4c69a05a4
furry <accept441@gmail.com>
2017-10-31 09:11:28 +0800

    add DAO

---
+++ src/main/java/tw/kewang/testserver/DAO/Service3_DAO_Response.java
@@ -0,0 +1,24 @@
+package tw.kewang.testserver.DAO;
+
+public class Service3_DAO_Response {
+    public String rsp_code;
+    public String rsp_msg;
+
+    public void setRsp_code(String rsp_code) {
+        this.rsp_code = rsp_code;
+    }
+
+    public String getRsp_code() {
+        return this.rsp_code;
+    }
+
+    public void setRsp_msg(String rsp_msg) {
+        this.rsp_msg = rsp_msg;
+    }
+
+    public String getRsp_msg() {
+        return this.rsp_msg;
+    }
+
+
+}
